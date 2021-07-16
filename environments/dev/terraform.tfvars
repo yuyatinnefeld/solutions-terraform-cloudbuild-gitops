8ae57c8d792359896588f7c6d6a9c39afef430a1
gcp <gcp@KOENB30581.koeln.ads.risnet.de>
2021-07-16 14:36:12 +0200

    Update project IDs and buckets

--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="gcp-terraform-320011"
--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "gcp-terraform-320011-tfstate"
     prefix = "env/prod"
   }
 }
